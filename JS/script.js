d6c6049a34f8df901728a3ec485cf86268a24d95
Carlos Aguire <carlosaguirre086@gmail.com>
2024-01-10 15:26:10 -0800

    added style to the portfolio section

--- JS/script.js
+++ JS/script.js
@@ -4,57 +4,75 @@ $(document).ready(function () {
     play: 5000,
     pagination: false,
   });
-});
 
-var typed = new Typed(".typed", {
-  strings: ["Software Engineer", "Web Developer", "Student"],
-  typeSpeed: 70,
-  loop: true,
-  startDelay: 1000,
-  showCursor: false,
-});
+  var typed = new Typed(".typed", {
+    strings: ["Software Engineer.", "Web Developer.", "Student."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false,
+  });
 
-$(".owl-carousel").owlCarousel({
-  loop: true,
-  items: 4,
-  responsive: {
-    0: {
-      items: 1,
-    },
-    480: {
-      items: 2,
-    },
-    768: {
-      items: 3,
-    },
-    938: {
-      items: 4,
-    },
-  },
-});
-
-var skillsTopOffset = $(".skillsSection").offset().top;
-
-$(window).scroll(function () {
-  if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-    $(".chart").easyPieChart({
-      easing: "easeInOut",
-      barColor: "#fff",
-      trackColor: false,
-      scaleColor: false,
-      lineWidth: 4,
-      size: 152,
-      onStep: function (from, to, percent) {
-        $(this.el).find(".percent").text(Math.round(percent));
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1,
       },
-    });
-  }
-});
+      480: {
+        items: 2,
+      },
+      768: {
+        items: 3,
+      },
+      938: {
+        items: 4,
+      },
+    },
+  });
 
-//Fix count up and the skills bar deprecated varialble.
-$(".counter").each(function () {
-  var element = $(this);
-  var endVal = parseInt(element.text());
+  var skillsTopOffset = $(".skillsSection").offset().top;
+  var statsTopOffset = $(".statsSection").offset().top;
+  var countUpFinished = false;
+  $(window).scroll(function () {
+    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
+      $(".chart").easyPieChart({
+        easing: "easeInOut",
+        barColor: "#fff",
+        trackColor: false,
+        scaleColor: false,
+        lineWidth: 4,
+        size: 152,
+        onStep: function (from, to, percent) {
+          $(this.el).find(".percent").text(Math.round(percent));
+        },
+      });
+    }
 
-  element.countup(endVal);
+    if (
+      !countUpFinished &&
+      window.pageYOffset > statsTopOffset - $(window).height() + 200
+    ) {
+      $(".counter").each(function () {
+        var element = $(this);
+        var endVal = parseInt(element.text());
+
+        element.countup(endVal);
+      });
+
+      countUpFinished = true;
+    }
+  });
+
+  $("[data-fancybox]").fancybox();
+
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 1500,
+      easing: "linear",
+      queue: false,
+    },
+  });
 });
