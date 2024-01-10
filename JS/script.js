fd471d4db22108cb98e944cf2065c63887cf31ba
Carlos Aguire <carlosaguirre086@gmail.com>
2024-01-09 16:09:46 -0800

    added about me section and technical skills

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,44 @@ $(document).ready(function () {
     pagination: false,
   });
 });
+
+var typed = new Typed(".typed", {
+  strings: ["Software Engineer", "Web Developer", "Student"],
+  typeSpeed: 70,
+  loop: true,
+  startDelay: 1000,
+  showCursor: false,
+});
+
+$(".owl-carousel").owlCarousel({
+  loop: true,
+  items: 4,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    480: {
+      items: 2,
+    },
+    768: {
+      items: 3,
+    },
+    938: {
+      items: 4,
+    },
+  },
+});
+
+$(function () {
+  $(".chart").easyPieChart({
+    easing: "easeInOut",
+    barColor: "#fff",
+    trackColor: false,
+    scaleColor: false,
+    lineWidth: 4,
+    size: 152,
+    onStep: function (from, to, percent) {
+      $(this.el).find(".percent").text(Math.round(percent));
+    },
+  });
+});
