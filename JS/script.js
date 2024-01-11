93c8db83ecf0df68258aa54ab3e22b8a810e1230
Carlos Aguire <carlosaguirre086@gmail.com>
2024-01-11 11:46:16 -0800

    Completed Nav bar, portfolio section

--- JS/script.js
+++ JS/script.js
@@ -32,47 +32,85 @@ $(document).ready(function () {
     },
   });
 
-  var skillsTopOffset = $(".skillsSection").offset().top;
-  var statsTopOffset = $(".statsSection").offset().top;
-  var countUpFinished = false;
-  $(window).scroll(function () {
-    if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
-      $(".chart").easyPieChart({
-        easing: "easeInOut",
-        barColor: "#fff",
-        trackColor: false,
-        scaleColor: false,
-        lineWidth: 4,
-        size: 152,
-        onStep: function (from, to, percent) {
-          $(this.el).find(".percent").text(Math.round(percent));
+  $(document).ready(function () {
+    var skillsTopOffset = $(".skillsSection").offset().top;
+    var statsTopOffset = $(".statsSection").offset().top;
+    var countUpFinished = false;
+
+    $(window).scroll(function () {
+      if (window.scrollY > skillsTopOffset - $(window).height() + 200) {
+        $(".chart").easyPieChart({
+          easing: "easeInOut",
+          barColor: "#fff",
+          trackColor: false,
+          scaleColor: false,
+          lineWidth: 4,
+          size: 152,
+          onStep: function (from, to, percent) {
+            $(this.el).find(".percent").text(Math.round(percent));
+          },
+        });
+      }
+
+      if (
+        !countUpFinished &&
+        window.scrollY > statsTopOffset - $(window).height() + 200
+      ) {
+        $(".counter").each(function () {
+          var element = $(this);
+          var endVal = parseInt(element.text());
+
+          element.countup(endVal);
+        });
+
+        countUpFinished = true;
+      }
+    });
+
+    $("[data-fancybox]").fancybox();
+
+    $(".items").isotope({
+      filter: "*",
+      animationOptions: {
+        duration: 1500,
+        easing: "linear",
+        queue: false,
+      },
+    });
+
+    $("#filters a").click(function () {
+      $("#filters.current").removeClass("current");
+      $(this).addClass("current");
+
+      var selector = $(this).attr("data-filter");
+
+      $(".items").isotope({
+        filter: selector,
+        animationOptions: {
+          duration: 1500,
+          easing: "linear",
+          queue: false,
         },
       });
-    }
 
-    if (
-      !countUpFinished &&
-      window.pageYOffset > statsTopOffset - $(window).height() + 200
-    ) {
-      $(".counter").each(function () {
-        var element = $(this);
-        var endVal = parseInt(element.text());
-
-        element.countup(endVal);
-      });
-
-      countUpFinished = true;
-    }
+      return false;
+    });
   });
 
-  $("[data-fancybox]").fancybox();
+  const nav = $("#navigation");
+  const navTop = nav.offset().top;
 
-  $(".items").isotope({
-    filter: "*",
-    animationOptions: {
-      duration: 1500,
-      easing: "linear",
-      queue: false,
-    },
-  });
+  $(window).on("scroll", stickyNavigation);
+
+  function stickyNavigation() {
+    var body = $("body");
+
+    if ($(window).scrollTop() >= navTop) {
+      body.css("padding-top", nav.outerHeight() + "px");
+      body.addClass("fixedNav");
+    } else {
+      body.css("padding-top", 0);
+      body.removeClass("fixedNav");
+    }
+  }
 });
